3c2ac76164a51f1621ba2f21fe1217f0abbeaf2f
behradsiamaki <145423707+behradsiamaki@users.noreply.github.com>
2024-01-23 11:40:41 +0330

    code

---
+++ app.js
@@ -0,0 +1,22 @@
+const form = document.querySelector('#form');
+const title = document.querySelector('#title');
+const price = document.querySelector('#price');
+const products = document.querySelector('#products');
+
+const addProduct = (event) => {
+    event.preventDefault();
+
+    const productItem = document.createElement('li');
+    const productInfo = document.createTextNode(`${title.value} - ${price.value}`);
+
+    console.log(productInfo);
+
+    productItem.appendChild(productInfo);
+    products.appendChild(productItem);
+
+    title.value = '';
+    price.value = '';
+
+}
+
+form.addEventListener('submit', addProduct)
